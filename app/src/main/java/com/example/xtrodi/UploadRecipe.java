d51cd0e78dd15cdbde04293ce72e7b21360576c4
naduni123 <nadunidilmika@gmail.com>
2021-03-14 15:43:50 +0530

    test uplode recipe

--- app/src/main/java/com/example/xtrodi/UploadRecipe.java
+++ app/src/main/java/com/example/xtrodi/UploadRecipe.java
@@ -6,33 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity {
-
+public class UploadRecipe extends AppCompatActivity {
     AutoCompleteTextView autoCompleteTextView;
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_upload_recipe);
 
         ImageView leftIcon = findViewById(R.id.left_icon);
         ImageView rightIcon = findViewById(R.id.right_icon);
         TextView txt = findViewById(R.id.text);
 
         autoCompleteTextView = findViewById(R.id.autoComplete);
-
         String []option={"Rice","Bakery","Juice","Pudding","Others"};
-
-
-
+        Context context;
+        ArrayAdapter arrayAdapter = new ArrayAdapter(context:this , R.layout.option_item,option);
+        int position;
+        autoCompleteTextView.setText(arrayAdapter.getItem(position:0).Object filter;
+        toString() , filter:false);
 
 
     }
