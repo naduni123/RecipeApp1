199c0cb95ccb4998c33c91eb312bab3b286699ec
naduni123 <nadunidilmika@gmail.com>
2021-03-19 20:06:59 +0530

    grosery list

---
+++ app/src/main/java/com/example/kavindu/AdditemsToList.java
@@ -0,0 +1,14 @@
+package com.example.kavindu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdditemsToList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
